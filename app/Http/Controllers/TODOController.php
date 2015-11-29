9c08cb2bbb760dd7dea89b0f5d17a31d85993fcd
sebastien <sebastien.desmet@hotmail.fr>
2015-11-29 14:17:51 +0100

    Log/Register

---
+++ app/Http/Controllers/TODOController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Http\Controllers\Controller;
+use App\Link;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Request;
+
+//use Illuminate\Http\Request;
+
+class TODOController extends Controller{
+
+
+    public function index(){
+
+        return view('index');
+    }
+
+
+
+
+
+
+
+}
